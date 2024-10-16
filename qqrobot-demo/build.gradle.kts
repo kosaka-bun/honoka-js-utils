9e72e1408f5260b8df75c820ddfe368718aed581
Kosaka Bun <kosaka-bun@qq.com>
2024-10-16 13:24:43 +0800

    Add qqrobot-demo

---
+++ qqrobot-demo/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.nio.charset.StandardCharsets
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    java
+    alias(libs.plugins.dependency.management)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.jpa)
+}
+
+group = "de.honoka.qqrobot"
+version = libs.versions.root.get()
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = sourceCompatibility
+}
+
+dependencies {
+    //Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    //机器人框架
+    implementation(libs.qqrobot.spring.boot.starter)
+    //一些杂项
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.honoka.utils)
+    listOf(
+        libs.lombok,
+        "org.springframework.boot:spring-boot-configuration-processor"
+    ).forEach {
+        compileOnly(it)
+        annotationProcessor(it)
+        testCompileOnly(it)
+        testAnnotationProcessor(it)
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = StandardCharsets.UTF_8.name()
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = java.sourceCompatibility.toString()
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
